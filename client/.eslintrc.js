3fb61a2176249b31327508ebacffdc17a3662c31
Ilya Nazarov <i.nazarov@bi.zone>
2022-03-20 23:02:32 +0300

    update deps

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'import/prefer-default-export': 'off',
+    'react/function-component-definition': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/no-array-index-key': 'warn',
     'react/no-unescaped-entities': 'warn',
